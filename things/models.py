70407c3f0ef066a78aa358f2fff1ea562a0bd111
arraf2211 <arrafrahman22@gmail.com>
2022-10-08 20:04:35 +0100

    added models

--- things/models.py
+++ things/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    name = models.TextField(blank = False)
+    description = models.TextField(blank = True)
+    quantity = models.IntegerField(blank = False)
