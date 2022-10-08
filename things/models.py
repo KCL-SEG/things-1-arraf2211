42dd2b32cba673fa3e6a0ece95adb916a714b635
arraf2211 <arrafrahman22@gmail.com>
2022-10-08 20:57:59 +0100

    added correct model

--- things/models.py
+++ things/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import RegexValidator
+from django.core.validators import MaxValueValidator
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
@@ -7,4 +7,4 @@ from django.contrib.auth.models import AbstractUser
 class Thing(AbstractUser):
     name = models.TextField(blank = False, max_length = 30)
     description = models.TextField(blank = True,max_length = 120)
-    quantity = models.IntegerField(blank = False, max_value = 100)
+    quantity = models.IntegerField(blank = False, MaxValueValidator(100))
