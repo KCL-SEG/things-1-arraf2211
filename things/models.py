8c6cafd487e22873209979f59f7336be016b756d
arraf2211 <arrafrahman22@gmail.com>
2022-10-08 20:47:17 +0100

    added correct model

--- things/models.py
+++ things/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import RegexValidator
+from django.core.validators import maxValueValidator
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
