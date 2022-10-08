e67dc941619dcde7434d9db95a10bd3f9b62ee74
arraf2211 <arrafrahman22@gmail.com>
2022-10-08 21:02:13 +0100

    added correct model

--- things/models.py
+++ things/models.py
@@ -7,4 +7,6 @@ from django.contrib.auth.models import AbstractUser
 class Thing(AbstractUser):
     name = models.TextField(blank = False, max_length = 30)
     description = models.TextField(blank = True,max_length = 120)
-    quantity = models.IntegerField(blank = False, MaxValueValidator(100))
+    quantity = models.IntegerField(blank = False,
+           validators=[ MaxValueValidator(100)]
+            )
