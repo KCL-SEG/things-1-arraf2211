fe506b7dee603fc8a7a510e71e036ab1a84d368a
arraf2211 <arrafrahman22@gmail.com>
2022-10-10 23:46:39 +0100

    added correct models

--- things/models.py
+++ things/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class Thing(models.Model):
+class Thing(AbstractUser):
     name = models.CharField(blank = False, max_length = 30, unique = True)
     description = models.CharField(max_length = 120, unique = False, blank = True)
     quantity = models.IntegerField(blank = False,
