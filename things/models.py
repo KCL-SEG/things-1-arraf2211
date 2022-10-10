6ab8fd9de338d388c776640f9f2ee597a079fef5
arraf2211 <arrafrahman22@gmail.com>
2022-10-10 23:27:27 +0100

    added correct fields

--- things/models.py
+++ things/models.py
@@ -1,12 +1,14 @@
 from django.db import models
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 class Thing(AbstractUser):
-    name = models.TextField(blank = False, max_length = 30)
+    name = models.TextField(blank = False, max_length = 30, unique = True)
     description = models.TextField(blank = True,max_length = 120)
     quantity = models.IntegerField(blank = False,
-           validators=[ MaxValueValidator(100)]
+           validators=[ MaxValueValidator(100),
+                        MinValueValidator(0)         
+               ]
             )
